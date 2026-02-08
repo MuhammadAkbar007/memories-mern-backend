aa735eb6535320432d4f6400a007529a4fdcffbc
MuhammadAkbar007 <fotismenos007@mail.ru>
2026-02-08 21:30:31 +0500

    resume 🥉

--- index.js
+++ index.js
@@ -1,27 +1,34 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import postsRouter from './routes/postRoutes.js'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import postsRouter from "./routes/postRoutes.js";
 
-const app = express()
-dotenv.config()
+const app = express();
+dotenv.config();
 
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 // const DB_USERNAME = 'fotismenos'
 // const DB_PASSWORD = 'fotismenos007'
 // const CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.qy6uw.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)))
-.catch(err => console.log(`${err} did not connect`))
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)),
+  )
+  .catch((err) => console.log(`${err} did not connect`));
 
 /* Ketma-ketlik muhim yuqoridagi configlar va pastdagi routelar */
-app.use('/posts', postsRouter)
-app.use('/', (req, res) => res.send('Bismillah, Memories API'))
+app.use("/posts", postsRouter);
+app.use("/", (_, res) => res.send("Bismillah, Memories API"));
+
